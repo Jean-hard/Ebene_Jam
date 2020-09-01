7c25f08c41c233fe058ab57890276c30a8c1a641
VincentB2 <hjklm96301@gmail.com>
2020-09-01 17:57:29 +0200

    Spawn_Humains_Gauche_1

---
+++ Ebene_Jam_Unity/Assets/Vincent/Scripts/Human_Left.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human_Left : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Vector3 Initialize = new Vector3(3, 0, 0);
+        transform.position = transform.position + Initialize * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 Initialize = new Vector3(1, 0, 0);
+        transform.position = transform.position + Initialize * Time.deltaTime;
+    }
+
+
+}
