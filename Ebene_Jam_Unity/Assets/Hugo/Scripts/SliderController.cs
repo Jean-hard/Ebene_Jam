29612dc1f095b03633cdf043c90b8ecec59f67a8
Hugo <hugo.tuil@edu.devinci.fr>
2020-09-01 18:55:08 +0200

    Parsec Sliders

---
+++ Ebene_Jam_Unity/Assets/Hugo/Scripts/SliderController.cs
@@ -0,0 +1,42 @@
+﻿using Rewired;
+using UnityEngine;
+
+namespace ParsecOverlaySample
+{
+    public class SliderController : MonoBehaviour
+    {
+        public string rewiredPlayerName = "Player0";
+        private Player _rewiredPlayer = null;
+
+        public float speed = 5f;
+
+        public float minX;
+        public float maxX;
+
+        private void Awake()
+        {
+            _rewiredPlayer = ReInput.players.GetPlayer(rewiredPlayerName);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 dir = _rewiredPlayer.GetAxis2D("MoveLeftRight", "MoveUpDown");
+
+            Vector3 newPosition = transform.position;
+            newPosition.x += dir.x * speed * Time.deltaTime;
+            if(newPosition.x <= minX)
+            {
+                newPosition.x = minX;
+            }
+            else if (newPosition.x >= maxX)
+            {
+                newPosition.x = maxX;
+            }
+            transform.position = newPosition;
+            Debug.Log(newPosition.x);
+        }
+    }
+}
+
+
---
+++ Ebene_Jam_Unity/Assets/Hugo/Scripts/SliderScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderScript : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public int player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = this.gameObject.GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == 1)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+
+                slider.value = slider.value - 1f;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+
+                slider.value = slider.value + 1f;
+            }
+        }
+        else if(player == 2)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+
+                slider.value = slider.value - 1f;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+
+                slider.value = slider.value + 1f;
+            }
+        }
+        
+    }
+}
