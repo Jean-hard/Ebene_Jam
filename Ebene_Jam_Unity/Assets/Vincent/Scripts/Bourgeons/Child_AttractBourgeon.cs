4aa711f0fb51be7156acd7eae2a6949f5c81c445
VincentB2 <hjklm96301@gmail.com>
2020-09-02 11:32:10 +0200

    Bourgeon_Attract_Vincent

---
+++ Ebene_Jam_Unity/Assets/Vincent/Scripts/Bourgeons/Child_AttractBourgeon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Child_AttractBourgeon : MonoBehaviour
+{
+    // Enfant du bourgeon Attract, il a pour rôle de stopper les humains au contact de son trigger.
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "HumanR")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+        if (collision.gameObject.tag == "HumanL")
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+}
