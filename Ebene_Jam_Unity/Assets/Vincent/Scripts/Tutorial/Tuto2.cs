517896fbe8384821d12347fc8444ffe6de1e94a7
VincentB2 <hjklm96301@gmail.com>
2020-09-03 17:15:36 +0200

    test

--- Ebene_Jam_Unity/Assets/Vincent/Scripts/Tutorial/Tuto2.cs
+++ Ebene_Jam_Unity/Assets/Vincent/Scripts/Tutorial/Tuto2.cs
@@ -6,15 +6,5 @@ public class Tuto2 : MonoBehaviour
 {
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
