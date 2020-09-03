7cd20bae2cbb6c9be6ed71fce35f3dff0af26c4c
Hugo <hugo.tuil@edu.devinci.fr>
2020-09-03 16:12:15 +0200

    FX

---
+++ Ebene_Jam_Unity/Assets/Hugo/Scripts/SFXManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+
+    public AudioClip[] effects = new AudioClip[5];
+
+    public AudioSource As;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameManager.hasEaten)
+        {
+            As.PlayOneShot(effects[0]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
