58a949c9bceab1c1e1c976f9f83d3438cd578b2e
VincentB2 <hjklm96301@gmail.com>
2020-09-03 16:14:48 +0200

    DebutTuto

---
+++ Ebene_Jam_Unity/Assets/Vincent/Scripts/Tutorial/Tuto1.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuto1 : MonoBehaviour
+{
+    public Animator black;
+    public Animator title;
+    public Animator text;
+    public Animator press;
+
+    public GameObject tutorial2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            black.SetTrigger("Move");
+            title.SetTrigger("Move");
+            text.SetTrigger("Move");
+            press.SetTrigger("Move");
+
+            Destroy(black.gameObject, 2);
+            Destroy(title.gameObject, 2);
+            Destroy(text.gameObject, 2);
+            Destroy(press.gameObject, 2);
+
+            StartCoroutine("NextStep");
+        }
+    }
+
+    IEnumerator NextStep()
+    {
+        yield return new WaitForSeconds(2);
+        tutorial2.SetActive(true);
+    }
+}
