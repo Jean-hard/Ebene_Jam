1939036c4aad0287d5e7fda11801e51fd76e89cb
JeanDev <hardyjea@gmail.com>
2020-09-04 11:38:48 +0200

    Add eat sound

--- Ebene_Jam_Unity/Assets/Hugo/Scripts/SFXManager.cs
+++ Ebene_Jam_Unity/Assets/Hugo/Scripts/SFXManager.cs
@@ -4,25 +4,7 @@ using UnityEngine;
 
 public class SFXManager : MonoBehaviour
 {
-
-    public AudioClip[] effects = new AudioClip[5];
+    public AudioClip[] effects = new AudioClip[6];
 
     public AudioSource As;
-    public GameManager gameManager;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (gameManager.hasEaten)
-        {
-            As.clip = effects[0];
-            As.Play();
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
